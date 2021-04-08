13e543bb634cd98c8fadd220a3797f6af08c6094
Maria Ortega <mariaortegapb@gmail.com>
2021-04-08 16:00:59 -0400

    added git ignore

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-Title of app:  README Generator
+# Title Readme Generator
+  ## Description
+  Description
+  ## Installation
+  node index.js
+  ## Usage
+  yes
+  ## Contribution
+  yes
+  ## Tests
+  yes
+  MIT
+   {
+   
+  }
+  maria
+  maria@gmail.com
 
-This app is not deployed a walkthrough video using Screencastify will be added.
-
-List of technologies used:  package.json, index.js file, .gitignore.
-
-1-2 sentences explaining what the app is: This app creates a README file using a command-line application. This app is invoked usiing node index.js
-
-2-3 Screenshots / gifs of the app: a walkthrough video using Screencastify will be added
-
-License (MIT)
-
-Contact information: my email: mariaortegapb@gmail.com
--- index.js
+++ index.js
@@ -1,83 +1,88 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 //const writeToFile = util
 
 // array of questions for user
-const questions = () => { 
-    return inquirer.prompt([
-    { 
-        type: 'input',  
-        name: 'title',  
-        message: 'What is the title of your project?',  
-        },  
-        {  
-        type: 'input',  
-        name: 'description',  
-        message: 'Describe your project:',  
-        },  
-        {  
-        type: 'input',  
-        name: 'installation',  
-        message: 'What are the installation instructions?',  
-        },  
-        {  
-        type: 'input',  
-        name: 'usage',  
-        message: 'Include usage information:',  
-        },  
-        {  
-        type: 'input',  
-        name: 'contribution',  
-        message: 'Include contribution guidelines:',  
-        },  
-        {  
-        type: 'input',  
-        name: 'test',  
-        message: 'Include test instructions.',  
-        },  
-        {  
-        type: 'list',  
-        name: 'license',  
-        message: 'Please select a license:', 
-        choices: ['MIT'],
-        },  
-        {  
-        type: 'input',  
-        name: 'username',  
-        message: 'Enter username:',  
-        },  
-        {  
-        type: 'input',  
-        name: 'email',  
-        message: 'Enter email:',  
-        },   
-]);
-}
-
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Describe your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the installation instructions?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Include usage information:",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Include contribution guidelines:",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Include test instructions.",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please select a license:",
+      choices: ["MIT"],
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter email:",
+    },
+  ]);
+};
 
 //For the data.license function to work correctly the license queston in the inquirer
 // must save a variable from a list of choices. Then, we will input that variable into
 // a conditionaly that will grab the relevant license and add it to our readme.
 
-
 // function to initialize program
 async function init() {
-    try { 
-        
-        
-    const answers = await questions(); 
-    const html = generateMarkdown(answers); 
-    console.log(html)
-    writeFile("ReadMe.md",generateMarkdown(answers))
-    } catch(error) {}
+  try {
+    const answers = await questions();
+    const html = generateMarkdown(answers);
+    console.log(html);
+    writeFile("ReadMe.md", generateMarkdown(answers));
+  } catch (error) {}
 }
 // function to write README file
-function writeFile(fileName, data) { 
-    return fs.writeFileSync(path.join(process.cwd(),fileName), data)
-
+function writeFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function call to initialize program
 init();
+
+// notes:
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
